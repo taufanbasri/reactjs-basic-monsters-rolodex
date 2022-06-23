a348aa222c0af53ecdce0060b5b96ca626a5aa10
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-06-23 08:17:22 +0700

    Shallow Merge

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Taufan Prasetyo",
+      name: { firstName: "Taufan", lastName: "Prasetyo" },
+      company: "Lubna Tech",
     };
   }
 
@@ -17,10 +18,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
+          </p>
           <button
             onClick={() => {
-              this.setState({ name: "Tamvan" });
+              this.setState({name: { firstName: "Babang", lastName: "Tamvan" }});
             }}
           >
             Change Name
