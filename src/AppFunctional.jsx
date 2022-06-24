ef80648627848a282c2a6fbc9ea98441eff3abfa
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-06-25 00:24:01 +0700

    Use Functional Components

---
+++ src/AppFunctional.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function AppFunctional() {
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+    </div>
+  );
+}
+
+export default AppFunctional;
