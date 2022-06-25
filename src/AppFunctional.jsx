4ef8b8990dd20b618ea825a0f3aad6084ee2f662
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-06-25 12:57:29 +0700

    Hooks: useEffect()

--- src/AppFunctional.jsx
+++ src/AppFunctional.jsx
@@ -1,9 +1,46 @@
+import { useState, useEffect } from "react";
+
 import "./App.css";
+import SearchBox from "./components/search-box/search-box.component";
+import CardList from "./components/card-list/card-list.component";
 
 function AppFunctional() {
+  console.log("render");
+  const [searchField, setSearchField] = useState(""); //[value, setvalue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
+
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="Search Monsters"
+      />
+
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
